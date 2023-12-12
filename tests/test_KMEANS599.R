af7526866a2b8814d12e26341f81729972b4591d
trevb11 <143556959+trevb11@users.noreply.github.com>
2023-12-12 01:09:32 -0800

    Create test_KMEANS599.R

---
+++ tests/test_KMEANS599.R
@@ -0,0 +1,11 @@
+library(testthat)
+library(devtools)
+install.packages('stats')
+
+test_that("KMEANS599 computs same answer as R", {
+  set.seed(17)
+  test.data <- matrix(rnorm(300), ncol = 2)
+  my.result <- KMEANS599(test.data, 2)
+  expected.result <- stats::kmeans(test.data)
+  expect_equal(my.result, expected.result)
+})
